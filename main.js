57a173969aa842fa24c87a9e6531532eb8ff778d
arindam31 <thearc31@gmail.com>
2019-12-22 13:52:08 +0100

    Launch page: Showing two buttons

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-let {PythonShell} = require('python-shell')
+var python_shell = require('python-shell')
+
 const {app, BrowserWindow} = require('electron')
 const { globalShortcut } = require('electron')
 
 
 function createWindow () {
-  globalShortcut.register('f5', function() {
-		console.log('f5 is pressed')
-		window.reload()
-	})
-    PythonShell.run('server.py',  function  (err, results)  {
-        if  (err)  console.log(err);
-       }); 
-       
+          globalShortcut.register('f5', function() {
+		      console.log('f5 is pressed')
+		      window.reload()
+                        })
+        
+          python_shell.PythonShell.run('server.py',  function  (err, results)  {
+          if  (err)  console.log(err);
+          }); 
+          
 
-    window = new BrowserWindow({width: 800, height: 600})
-    window.loadFile('index.html')
+          window = new BrowserWindow({width: 800, height: 600})
+          window.loadFile('index.html')
+          // window.loadURL('127.0.0.1:5000/home')
     
   }
 
   app.on('ready', createWindow)
+
+  
+  // app.on('window-all-closed', () => {
+  //   app.quit()
+  //   kill(python_shell.PythonShell.pid);
+  //    })  
